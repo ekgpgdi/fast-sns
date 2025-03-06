efa92c411b06e51e57ec1bb2b0f6508cc7930e5c
ekgpgdi <dlekgp0423@naver.com>
2025-03-06 18:05:41 +0900

    Initial commit

---
+++ src/test/java/dahye/fastsns/fastsns/FastsnsApplicationTests.java
@@ -0,0 +1,13 @@
+package dahye.fastsns.fastsns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastsnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
